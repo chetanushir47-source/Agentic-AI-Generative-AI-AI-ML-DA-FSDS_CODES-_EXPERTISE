959a74e46f83ec100bba245f946b3631db596904
chetanushir47-source <chetanushir47@gmail.com>
2026-01-06 11:05:45 +0530

    Add files via upload

---
+++ knn_classifier.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv(r"D:\DATASETS\logit classification.csv")
+
+x= df.iloc[:,[2,3]].values
+y= df.iloc[:,-1].values
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc= StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test= sc.transform(x_test)
+
+from sklearn.neighbors import KNeighborsClassifier
+cl= KNeighborsClassifier()
+cl.fit(x_train,y_train)
+
+y_pred= cl.predict(x_test)
+
+from sklearn.metrics import accuracy_score
+ac= accuracy_score(y_test,y_pred)
+
+from sklearn.svm import SVC
+classifier= SVC()
+classifier.fit(x_train ,y_train)
+bias = classifier.score(x_train ,y_train)
+print(bias)
+
+from sklearn.metrics import accuracy_score
+ac= accuracy_score(y_test,y_pred)
+print(ac) 
+
+                                    
+
+
